9db54b1cdcc1f2dfe77b6d63b0a880a6cd16022d
aereal <aereal@aereal.org>
2014-05-21 23:20:03 +0900

    Plack::Middleware::GitPunchCard mounts Plack::App::GitPunchCard::SeriesData

--- lib/Plack/Middleware/GitPunchCard.pm
+++ lib/Plack/Middleware/GitPunchCard.pm
@@ -2,67 +2,28 @@ package Plack::Middleware::GitPunchCard;
 use strict;
 use warnings;
 use parent qw( Plack::Middleware );
+use Plack::App::GitPunchCard::SeriesData;
 use Plack::Util::Accessor qw( path json_path max_log_count git_dir );
-use JSON::XS qw( encode_json );
-use List::UtilsBy qw( count_by );
 
 sub prepare_app {
     my ($self) = @_;
-    $self->json_path($self->path . '.json') unless defined $self->json_path;
-    $self->git_dir('./.git') unless defined $self->git_dir;
+    $self->{series_data} //= Plack::App::GitPunchCard::SeriesData->new(
+      max_log_count => $self->max_log_count,
+      git_dir => $self->git_dir // './.git',
+    );
 }
 
 sub call {
     my ($self, $env) = @_;
-    if ($env->{PATH_INFO} eq $self->json_path) {
-        my $series = $self->series_data($self->commit_datetime);
-        my $body = encode_json($series);
-        return [200, ['Content-Type' => 'application/json', 'Content-Length' => length($body)], [$body]];
-    } elsif ($env->{PATH_INFO} eq $self->path) {
+    my $path = $env->{PATH_INFO};
+    if ($path eq $self->json_path) {
+        return $self->{series_data}->call($env);
+    } elsif ($path eq $self->path) {
         my $body = $self->html;
         return [200, ['Content-Type' => 'text/html; charset=utf-8', 'Content-Length' => length($body)], [$body]];
     } else {
         return $self->app->($env);
     }
-}
-
-sub series_data {
-    my ($self, $times) = @_;
-    my %count_by_day = count_by { $self->_serialize_time($_)} @$times;
-    [ map {
-        my $t = $self->_deserialize_time($_);
-        ['', 0+$t->{hour}, 1+$t->{weekday}, 0+$count_by_day{$_}, 0+$count_by_day{$_}]
-    } keys %count_by_day ];
-}
-
-sub commit_datetime {
-    my ($self) = @_;
-    my $limit_opt = $self->git_log_limit_opt;
-    my $out = `git --no-pager log --format='%at' --no-merges $limit_opt`;
-    my @lines = split "\n", $out;
-    [ map { $self->_build_time_from_epoch($_) } @lines ];
-}
-
-sub git_log_limit_opt {
-    my ($self) = @_;
-    defined($self->max_log_count) ? '-n ' . $self->max_log_count : '';
-}
-
-sub _build_time_from_epoch {
-    (undef, my $epoch) = @_;
-    my ($hour, $weekday) = (localtime($epoch))[2, 6];
-    +{ hour => $hour, weekday => $weekday };
-}
-
-sub _serialize_time {
-    (undef, my $t) = @_;
-    $t->{hour} . ':' . $_->{weekday}
-}
-
-sub _deserialize_time {
-    (undef, my $key) = @_;
-    my ($hour, $weekday) = split ':', $key;
-    +{ hour => $hour, weekday => $weekday };
 }
 
 our $html = <<'EOF';
